db75d7df92d25b89e7669887d26526c63d22fdd5
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-04 22:43:12 -0400

    Add MongoDB connection logic, connection events and connection error handling into Models/farm.js

--- Models/farm.js
+++ Models/farm.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose');
 
+// Connection events
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
+
+// Connecting to MongoDB with the mongoose.connect() method
+mongoose.connect('mongodb://127.0.0.1:27017/relationshipDB')
+
+// Connection error handling
+.then(() => {console.log("Mongo connection is open!")})
+.catch((err) => {
+    console.log("Oh no, Mongo connection ERROR!!!");
+    console.log(err);
+})
